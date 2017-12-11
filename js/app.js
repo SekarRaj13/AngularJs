5515ef9ce07b917e5c207e1b215740dbb507a5a1
Ganasekaran <ganasekaran.r@sastratechnologies.in>
2017-12-11 11:52:19 +0530

    Angularjs learing

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var appModelName1 =angular.module("appModelName",[]);
+appModelName1.controller('appController',['$scope',  function($scope){
+	$scope.firstname = "sekar";
+	$scope.lastName = "raj";
+
+}]);
